eeb201d00cf6dcd1e7e2bdc4fea97796019cc47a
JakaPresecnik <jaka.presecnik@gmail.com>
2019-12-24 17:45:28 +0100

    1. Added retriveData to nameChecker.js 2. Added retriveData call and made dom manipulation in formHandler.js 3. Updated exports in index.js

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -12,5 +12,14 @@ function checkForName(inputText) {
         alert("Welcome, Captain!")
     }
 }
-
-export { checkForName }
+//1.asynchronous function to get the data from openweathermap with parameters entered by user
+const retriveData = async(baseURL, zip, country, apiKey) => {
+  const res = await fetch (baseURL+zip+country+apiKey)
+  try {
+    const data = await res.json();
+    return data;
+  }catch (error) {
+    console.log('error', error);
+  }
+}
+export { retriveData, checkForName }
