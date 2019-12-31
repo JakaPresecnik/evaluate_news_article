d3af0f17b64f7f9fffcc1b66831b31b0524a9071
JakaPresecnik <jaka.presecnik@gmail.com>
2019-12-31 10:16:41 +0100

    jest

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,6 +1,17 @@
 import { checkForName } from "../src/client/js/nameChecker"
+import { handleSubmit } from "../src/client/js/formHandler"
 
-describe("Testing the API call", () => {
-  test("Testing the API call", async () => {
+describe("Testing the API call function", () => {
+  test("Testing that API call function is defined", async () => {
     expect(checkForName).toBeDefined()
-  })})
+  })
+  test('Testing that the data returned is an object', async () => {
+     const data = await checkForName;
+     expect(data).toBeInstanceOf(Object)
+  })
+})
+
+describe("Testing the submit function", () => {
+  test("Testing the submit function", () => {
+    expect(handleSubmit).toBeDefined()
+})})
